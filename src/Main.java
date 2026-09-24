db64261f5fc7bc494f3ae7d6e826d55874030fb9
Jesus Naranjo <jesusn0421@gmail.com>
2026-09-24 13:20:03 -0700

    This may be how the menu looks in the end

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,50 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Movie movie1 = new Movie("Interstellar", "Sci-Fi", 2014);
-        Movie movie2 = new Movie("Backrooms", "Horror", 2026);
-// I'm not keeping main like this, this is me largely testing a singly linked list pointing
-        MovieNode node1 = new MovieNode(movie1);
-        MovieNode node2 = new MovieNode(movie2);
+        Scanner input = new Scanner(System.in);
+        int choice = 0;
 
-        node1.setNext(node2); 
+        while (choice != 4) {
+            System.out.println("Movie Manager\n");
+            System.out.println("1. Search Movies");
+            System.out.println("2. View Watchlist:");
+            System.out.println("3. View Watched List:");
+            System.out.println("4. Exit:");
+            System.out.print("Enter your choice: ");
 
-        System.out.println(node1.getMovie());
-        System.out.println(node1.getNext().getMovie());
+            String userInput = input.nextLine().trim();
+
+            if (userInput.endsWith(".")) {
+                userInput = userInput.substring(0, userInput.length() - 1);
+            }
+
+            try {
+                choice = Integer.parseInt(userInput);
+            } 
+            catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number from 1 to 4");
+                continue;
+            }
+            
+
+            if (choice == 1) {
+                System.out.println("Search Movies option selected");
+            }
+            else if (choice == 2) {
+                System.out.println("View Watchlist option selected");
+            }
+            else if (choice == 3) {
+                System.out.println("View Watched list option selected");
+            }
+            else if (choice == 4) {
+                System.out.println("Exiting Movie Manager...");
+            }
+            else {
+                System.out.println("Invalid option. Please type any of the numbers listed above"); 
+            }
+        }
+
+        input.close();
     }
 }
